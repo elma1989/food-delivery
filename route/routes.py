8fc3f2ed7264108cf6944dfca013731051b4a7f2
Marco Elste <marco.elste@web.de>
2025-07-12 19:25:55 +0200

    feat: Updated: route/routes.py: items()

--- route/routes.py
+++ route/routes.py
@@ -1,7 +1,14 @@
 from flask import Blueprint, render_template
+from database import Manager
 
 api = Blueprint('api',__name__)
 
 @api.route('/')
 def index():
     return render_template('index.html')
+
+@api.route('/items')
+def items():
+    mgr = Manager()
+    items = mgr.items
+    return [item.to_dict() for item in items]
--- test/test_06_route.py
+++ test/test_06_route.py
@@ -4,3 +4,30 @@ def test_index(url):
     req = requests.get(url)
 
     assert req.status_code == 200
+
+def test_items(url):
+    req = requests.get(url + 'items')
+
+    assert req.status_code == 200
+    data =  req.json()
+    assert data == [
+        {
+            'id': 3,
+            'name': 'Spaghetti',
+            'type': 'pasta',
+            'desc': 'Spaghetti Napolli',
+            'price': 5
+        }, {
+            'id': 1,
+            'name': 'Pizza Margherita',
+            'type': 'pizza',
+            'desc': 'Pizza mit Mozerella und Tomate',
+            'price': 4
+        }, {
+            'id': 2,
+            'name': 'Pizza Salami',
+            'type': 'pizza',
+            'desc': 'Pizza mit Salami',
+            'price': 7
+        },
+    ]
