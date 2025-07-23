ce30e7bd58694210c8dc44b092f4675cace0ea95
Marco Elste <marco.elste@web.de>
2025-07-23 13:54:02 +0200

    fix: Upadated template.js: Removed User-dependent Methods

--- static/templates.js
+++ static/templates.js
@@ -1,52 +1,5 @@
 export class Template{
     // #region Methods
-    static errMsg(msg) {
-        return /*html*/`
-            <div class="status-msg">Fehler: ${msg}</div>
-        `
-    }
-
-    static li(desc) {
-        return /*html*/`
-            <li><button>${desc}</button></li>
-        `
-    }
-
-    static greating() {
-        return /*html*/`
-            <h1>Herzlich Willkommen in der Bestell-App.</h1>
-        `
-    }
-    
-    static form(name) {
-        return /*html*/`
-            <form>
-                <fieldset>
-                    <legend>${name}</legend>
-                </fieldset>
-            </form>
-            <div class="status-msg"></div>
-        `
-    }
-    // #region Form
-    static forminput(name, id, type='text') {
-        return /*html*/`
-            <div class="formdata">
-                <label for="${id}">${name}: </label>
-                <input type="${type}" name="${id}" id="${id}">
-                <div class="errmsg"></div>
-            </div>
-        `
-    }
-
-    static formsubmit(submitname) {
-        return /*html*/`
-            <div class="formsubmit">
-                <input type="submit" value="${submitname}">
-            </div>
-        `
-    }
-    // #endregion
     // #region Dish
     static shelf (name, img) {
         return /*html*/`
